41d6cad5b1c8f16119c518c1e76c2c9f4c33f357
david-AGSGeo <David.Lee@AGSGeo.eu>
2015-10-26 19:13:59 +1100

    Root returns page

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var DEFAULT_BLOCK_TIME = 5*ONE_MINUTE;
 
 var hardware = require('./hardware.js');
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/static', express.static('static'))
-app.use('/', express.static('static/page.html'));
 
 var indoorSensor = hardware.getInternalTempSensor();
 var outdoorSensor = hardware.getExternalTempSensor();
@@ -84,6 +85,20 @@ app.route('/state')
 		}
 	});
 
+app.route('/')
+	.get(function(req, res) {
+		fs.readFile('static/page.html', 'utf8', function(err, data){
+			if (err) {
+				console.log("Could not load page.html");
+				
+				res.status(404).send('Not found');
+				return;
+			}
+			
+			res.end(data);
+		}))
+	});
+	
 app.listen(80, function() {
 	console.log("Listening");
 });
